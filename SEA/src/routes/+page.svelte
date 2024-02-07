5cc0b9c0154013325b094d452c1af0fa5017b72f
Prin-P <prinpulkes@gmail.com>
2024-02-07 00:36:52 -0500

    idk

--- SEA/src/routes/+page.svelte
+++ SEA/src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <h1>Hello, please enter your name and either create or join a planning room!</h1>
 <script>
     //import Button from "../components/Button.svelte"
-    import Button from '@smui/button';
+    //import Button from '@smui/button';
+    //import { Button } from '@skeletonlabs/skeleton';
 	let name = 'Enter Name';
     let joinRoom = 'Code to Join Room';
     let createRoom = 'Create Room Code';
@@ -9,7 +10,7 @@
 <input bind:value={name} />
 <input bind:value={joinRoom} />
 <input bind:value={createRoom} />
-<Button on:click={()=>console.log("Clicked")}>
+<button on:click={()=>console.log("Clicked")}>
     <a href="/calendar">Continue</a>
-</Button>
+</button>
 <p>If you are creating a room, please create a passcode and share it with your planees.</p>
