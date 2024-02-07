e10819fd1345e3009b116014e507a65aca719ee3
Prin-P <prinpulkes@gmail.com>
2024-02-06 21:29:22 -0500

    not working

--- SEA/src/routes/calendar/+page.svelte
+++ SEA/src/routes/calendar/+page.svelte
@@ -2,13 +2,14 @@
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@event-calendar/build@2.6.1/event-calendar.min.css">
 
-<script src="https://cdn.jsdelivr.net/npm/@event-calendar/build@2.6.1/event-calendar.min.js">
+
+
+<script src="https://cdn.jsdelivr.net/npm/@event-calendar/build@2.6.1/event-calendar.min.js" type="text/javascript">
     import Calendar from '@event-calendar/core';
     import TimeGrid from '@event-calendar/time-grid';
     import Interaction from '@event-calendar/interaction';
     import '@event-calendar/core/index.css';
     import { browser } from '$app/environment';
-
     let cal;
     let plugins = [TimeGrid, Interaction];
     let options = {
@@ -20,24 +21,28 @@
         selectable: true,
 
         select: function (info) {cal.addEvent(info)}, // cal.add event to confirm an event to add
+        eventClick: function (info) { removeEvent(info)}
         //dateClick: function (info) {cal.addEvent(info);},
-       //eventDragStart: function (info) {console.log('dragStart');},
+    //eventDragStart: function (info) {console.log('dragStart');},
         //eventDragStop: function (info) {console.log('dragStop');},
         //eventDrop: function (info) {console.log('drop');},
     };
 
-    
-
-    function createEvents() {
+    function removeEvent(){
+        cal.removeEventById(info.event.id)
+    }
+    export function createEvents() {
         let days = [];
         for (let i = 0; i < 7; ++i) {
             let day = new Date();
             let diff = i - day.getDay();
             day.setDate(day.getDate() + diff);
+            year = day.getFullYear()
+            days[i] = "${year}"
             days[i] = day.getFullYear() + "-" + _pad(day.getMonth()+1) + "-" + _pad(day.getDate());
         }
 
-        return [
+        events = [
             {start: days[0] + " 00:00", end: days[0] + " 09:00", resourceId: 1, display: "background"},
             {start: days[1] + " 12:00", end: days[1] + " 14:00", resourceId: 2, display: "background"},
             {start: days[2] + " 17:00", end: days[2] + " 24:00", resourceId: 1, display: "background"},
@@ -51,126 +56,128 @@
             {start: days[5] + " 18:00", end: days[5] + " 21:00", resourceId: 2, title: "", color: "#B29DD9"},
             {start: days[1], end: days[3], resourceId: 1, title: "All-day events can be displayed at the top", color: "#B29DD9", allDay: true}
         ];
+
+        events.forEach(el => {
+            alert(el)
+            cal.addEvent(el)
+        })
     }
 
     function _pad(num) {
         let norm = Math.floor(Math.abs(num));
         return (norm < 10 ? '0' : '') + norm;
     }
+    /* exported gapiLoaded */
+    /* exported gisLoaded */
+    /* exported handleAuthClick */
+    /* exported handleSignoutClick */
 
-</script>
+    // TODO(developer): Set to client ID and API key from the Developer Console
+    const CLIENT_ID = '591983148481-03ba760usd8mv1534pr6b83l4l94hjup.apps.googleusercontent.com';
+    const API_KEY = 'AIzaSyB_l63gYXH3mQw8G3PIDpy9AZbg7rJziWY';
 
-    {#if browser}
-        <script type="text/javascript">
-        /* exported gapiLoaded */
-        /* exported gisLoaded */
-        /* exported handleAuthClick */
-        /* exported handleSignoutClick */
+    // Discovery doc URL for APIs used by the quickstart
+    const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
 
-        // TODO(developer): Set to client ID and API key from the Developer Console
-        const CLIENT_ID = '591983148481-03ba760usd8mv1534pr6b83l4l94hjup.apps.googleusercontent.com';
-        const API_KEY = 'AIzaSyB_l63gYXH3mQw8G3PIDpy9AZbg7rJziWY';
+    // Authorization scopes required by the API; multiple scopes can be
+    // included, separated by spaces.
+    const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly';
 
-        // Discovery doc URL for APIs used by the quickstart
-        const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest';
+    let tokenClient;
+    let gapiInited = false;
+    let gisInited = false;
 
-        // Authorization scopes required by the API; multiple scopes can be
-        // included, separated by spaces.
-        const SCOPES = 'https://www.googleapis.com/auth/calendar.readonly';
+    document.getElementById('authorize_button').style.visibility = 'hidden';
+    document.getElementById('signout_button').style.visibility = 'hidden';
 
-        let tokenClient;
-        let gapiInited = false;
-        let gisInited = false;
-
-        document.getElementById('authorize_button').style.visibility = 'hidden';
-        document.getElementById('signout_button').style.visibility = 'hidden';
-
-        /**
-         * Callback after api.js is loaded.
-         */
-        function gapiLoaded() {
+    /**
+     * Callback after api.js is loaded.
+     */
+    function gapiLoaded() {
         gapi.load('client', initializeGapiClient);
-        }
+    }
 
-        /**
-         * Callback after the API client is loaded. Loads the
-         * discovery doc to initialize the API.
-         */
-        async function initializeGapiClient() {
-        await gapi.client.init({
-            apiKey: API_KEY,
-            discoveryDocs: [DISCOVERY_DOC],
-        });
-        gapiInited = true;
-        maybeEnableButtons();
-        }
+    /**
+     * Callback after the API client is loaded. Loads the
+     * discovery doc to initialize the API.
+     */
+    async function initializeGapiClient() {
+    await gapi.client.init({
+        apiKey: API_KEY,
+        discoveryDocs: [DISCOVERY_DOC],
+    });
+    gapiInited = true;
+    maybeEnableButtons();
+    }
 
-        /**
-         * Callback after Google Identity Services are loaded.
-         */
-        function gisLoaded() {
-        tokenClient = google.accounts.oauth2.initTokenClient({
-            client_id: CLIENT_ID,
-            scope: SCOPES,
-            callback: '', // defined later
-        });
-        gisInited = true;
-        maybeEnableButtons();
-        }
+    /**
+     * Callback after Google Identity Services are loaded.
+     */
+    function gisLoaded() {
+    tokenClient = google.accounts.oauth2.initTokenClient({
+        client_id: CLIENT_ID,
+        scope: SCOPES,
+        callback: '', // defined later
+    });
+    gisInited = true;
+    maybeEnableButtons();
+    }
 
-        /**
-         * Enables user interaction after all libraries are loaded.
-         */
-        function maybeEnableButtons() {
-        if (gapiInited && gisInited) {
-            document.getElementById('authorize_button').style.visibility = 'visible';
-        }
-        }
+    /**
+     * Enables user interaction after all libraries are loaded.
+     */
+    function maybeEnableButtons() {
+    if (gapiInited && gisInited) {
+        document.getElementById('authorize_button').style.visibility = 'visible';
+    }
+    }
 
-        /**
-         *  Sign in the user upon button click.
-         */
-        function handleAuthClick() {
-        tokenClient.callback = async (resp) => {
-            if (resp.error !== undefined) {
-            throw (resp);
-            }
-            document.getElementById('signout_button').style.visibility = 'visible';
-            document.getElementById('authorize_button').innerText = 'Refresh';
-            await listUpcomingEvents();
-        };
+    /**
+     *  Sign in the user upon button click.
+     */
+    function handleAuthClick() {
+    tokenClient.callback = async (resp) => {
+        if (resp.error !== undefined) {
+        throw (resp);
+        }
+        document.getElementById('signout_button').style.visibility = 'visible';
+        document.getElementById('authorize_button').innerText = 'Sync another calendar';
+        createEvents()
+        //await listUpcomingEvents();
+    };
 
-        if (gapi.client.getToken() === null) {
-            // Prompt the user to select a Google Account and ask for consent to share their data
-            // when establishing a new session.
-            tokenClient.requestAccessToken({prompt: 'consent'});
-        } else {
-            // Skip display of account chooser and consent dialog for an existing session.
-            tokenClient.requestAccessToken({prompt: ''});
-        }
-        }
+    if (gapi.client.getToken() === null) {
+        // Prompt the user to select a Google Account and ask for consent to share their data
+        // when establishing a new session.
+        tokenClient.requestAccessToken({prompt: 'consent'});
+    } else {
+        // Skip display of account chooser and consent dialog for an existing session.
+        tokenClient.requestAccessToken({prompt: ''});
+    }
+    }
 
-        /**
-         *  Sign out the user upon button click.
-         */
-        function handleSignoutClick() {
-        const token = gapi.client.getToken();
-        if (token !== null) {
-            google.accounts.oauth2.revoke(token.access_token);
-            gapi.client.setToken('');
-            document.getElementById('content').innerText = '';
-            document.getElementById('authorize_button').innerText = 'Authorize';
-            document.getElementById('signout_button').style.visibility = 'hidden';
-        }
-        }
+    /**
+     *  Sign out the user upon button click.
+     */
+    function handleSignoutClick() {
+    const token = gapi.client.getToken();
+    if (token !== null) {
+        google.accounts.oauth2.revoke(token.access_token);
+        gapi.client.setToken('');
+        document.getElementById('content').innerText = '';
+        document.getElementById('authorize_button').innerText = 'Authorize';
+        document.getElementById('signout_button').style.visibility = 'hidden';
+    }
+    }
 
-        /**
-         * Print the summary and start datetime/date of the next ten events in
-         * the authorized user's calendar. If no events are found an
-         * appropriate message is printed.
-         */
-        async function listUpcomingEvents() {
+    /**
+     * Print the summary and start datetime/date of the next ten events in
+     * the authorized user's calendar. If no events are found an
+     * appropriate message is printed.
+     */
+    async function listUpcomingEvents() {
         let response;
+        //createEvents()
         try {
             const request = {
             'calendarId': 'primary',
@@ -180,7 +187,9 @@
             'maxResults': 10,
             'orderBy': 'startTime',
             };
-            response = await gapi.client.calendar.events.list(request);
+            
+            //response = await gapi.client.calendar.events.list(request);
+            
         } catch (err) {
             document.getElementById('content').innerText = err.message;
             return;
@@ -196,14 +205,28 @@
             (str, event) => `${str}${event.summary} (${event.start.dateTime || event.start.date})\n`,
             'Events:\n');
         document.getElementById('content').innerText = output;
-        }
-    </script>
-  {/if}
-  {#if browser}
-    <script async defer src="https://apis.google.com/js/api.js" onload="gapiLoaded()"></script>
-  {/if}
-  {#if browser}
-    <script async defer src="https://accounts.google.com/gsi/client" onload="gisLoaded()"></script>
-  {/if}
+    }
 
-<Calendar bind:this={cal} {plugins} {options} />
+</script>
+{#if browser}
+    <script type="text/javascript">
+        
+    </script>
+{/if}
+{#if browser}
+    <script async defer src="https://apis.google.com/js/api.js" onload="gapiLoaded()"></script>
+{/if}
+{#if browser}
+    <script async defer src="https://accounts.google.com/gsi/client" onload="gisLoaded()"></script>
+{/if}
+
+  <header class="row">
+    <h4 class="col"><a href="https://github.com/vkurko/calendar">Event Calendar</a> Demo</h4>
+        <button id="authorize_button" on:click={handleAuthClick}> Authorize</button>
+        <button id="authorize_button" on:click={handleSignoutClick}> Sign Out</button>
+    </header>
+    <main class="row">
+        <Calendar bind:this={cal} {plugins} {options} />
+    </main>
+    
+
