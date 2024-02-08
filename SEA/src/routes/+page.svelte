bf2b0ae2bfa0d60439d2962968b8812eeb89205a
Prin-P <prinpulkes@gmail.com>
2024-02-08 13:02:40 -0500

    removed excess code

--- SEA/src/routes/+page.svelte
+++ SEA/src/routes/+page.svelte
@@ -1,16 +1,13 @@
 <h1>Hello, please enter your name and either create or join a planning room!</h1>
 <script>
-    //import Button from "../components/Button.svelte"
-    //import Button from '@smui/button';
-    //import { Button } from '@skeletonlabs/skeleton';
-	let name = 'Enter Name';
-    let joinRoom = 'Code to Join Room';
-    let createRoom = 'Create Room Code';
+	let name = '';
+    let joinRoom = '';
+    let createRoom = '';
 </script>
-<input bind:value={name} />
-<input bind:value={joinRoom} />
-<input bind:value={createRoom} />
+<input bind:value={name} placeholder='Enter Name'/>
+<input bind:value={joinRoom} placeholder='Code to Join Room'/>
+<input bind:value={createRoom} placeholder='Create Room Code'/>
 <button on:click={()=>console.log("Clicked")}>
     <a href="/calendar">Continue</a>
 </button>
-<p>If you are creating a room, please create a passcode and share it with your planees.</p>
+<p>If you are creating a room, please create a room code and share it with your planees.</p>
--- SEA/src/routes/calendar/+page.svelte
+++ SEA/src/routes/calendar/+page.svelte
@@ -28,64 +28,10 @@
         cal.removeEventById(info.event.id)
     }
 
-    function createEvents() {
-        //alert("test")
-        //let day = new Date(2024, 2, 7, 10, 33, 30, 0);
-        //let day2 = new Date(2024, 2, 7, 16, 33, 30, 0);
-        let days = [];
-        for (let i = 0; i < 7; ++i) {
-            let day = new Date();
-            let diff = i - day.getDay();
-            day.setDate(day.getDate() + diff);
-            days[i] = day.getFullYear() + "-" + _pad(day.getMonth()+1) + "-" + _pad(day.getDate());
-        }
-
-        return [
-            {start: days[0] + " 00:00", end: days[0] + " 09:00", resourceId: 1, display: "background"},
-            {start: days[1] + " 12:00", end: days[1] + " 14:00", resourceId: 2, display: "background"},
-            {start: days[2] + " 17:00", end: days[2] + " 24:00", resourceId: 1, display: "background"},
-            {start: days[0] + " 10:00", end: days[0] + " 14:00", resourceId: 1, title: "The calendar can display background and regular events", color: "#FE6B64"},
-            {start: days[1] + " 16:00", end: days[2] + " 08:00", resourceId: 2, title: "An event may span to another day", color: "#B29DD9"},
-            {start: days[2] + " 09:00", end: days[2] + " 13:00", resourceId: 2, title: "Events can be assigned to resources and the calendar has the resources view built-in", color: "#779ECB"},
-            {start: days[3] + " 14:00", end: days[3] + " 20:00", resourceId: 1, title: "", color: "#FE6B64"},
-            {start: days[3] + " 15:00", end: days[3] + " 18:00", resourceId: 1, title: "Overlapping events are positioned properly", color: "#779ECB"},
-            {start: days[5] + " 10:00", end: days[5] + " 16:00", resourceId: 2, title: "You have complete control over the <i><b>display</b></i> of events…", color: "#779ECB"},
-            {start: days[5] + " 14:00", end: days[5] + " 19:00", resourceId: 2, title: "…and you can drag and drop the events!", color: "#FE6B64"},
-            {start: days[5] + " 18:00", end: days[5] + " 21:00", resourceId: 2, title: "", color: "#B29DD9"},
-            {start: days[1], end: days[3], resourceId: 1, title: "All-day events can be displayed at the top", color: "#B29DD9", allDay: true}
-        ];
-
-        events.forEach(el => {
-            alert(el)
-            cal.addEvent(el)
-        })
-    }
-
     function _pad(num) {
         let norm = Math.floor(Math.abs(num));
         return (norm < 10 ? '0' : '') + norm;
     }
-    
-    /*const start = async () => {
-      // Initializes the client with the API key and the Translate API.
-      // @ts-ignore
-      gapi.client.init({
-        'apiKey': 'AIzaSyB_l63gYXH3mQw8G3PIDpy9AZbg7rJziWY',
-        'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'],
-      }).then(function() {
-        // Executes an API request, and returns a Promise.
-        // The method name `language.translations.list` comes from the API discovery.
-        return gapi.client.language.translations.list({
-          q: 'hello world',
-          source: 'en',
-          target: 'de',
-        });
-      }).then(function(response) {
-        console.log(response.result.data.translations[0].translatedText);
-      }, function(reason) {
-        console.log('Error: ' + reason.result.error.message);
-      });
-    };*/
 
     const CLIENT_ID = '591983148481-03ba760usd8mv1534pr6b83l4l94hjup.apps.googleusercontent.com';
     const API_KEY = 'AIzaSyB_l63gYXH3mQw8G3PIDpy9AZbg7rJziWY';
