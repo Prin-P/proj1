815d22a1f6a82e04c976c1cd087f821e6d9b2418
Prin-P <prinpulkes@gmail.com>
2024-02-08 22:33:43 -0500

    Delete Dialog.svelte

--- SEA/src/components/Dialog.svelte
+++
@@ -1,7 +0,0 @@
-<!--Adapted from https://svelte.dev/repl/885653f873284f7880490dcdd1200238?version=3.48.0-->
-<script>
-	export let dialog
-</script>
-<dialog bind:this={dialog} on:close>
-	<slot/>
-</dialog>
